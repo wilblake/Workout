b4f102f634823cae0633ac570056ed68d2e6e8c2
wil <wblake@emsys.net>
2019-08-16 21:10:15 -0400

    First Pass simple

--- app/src/main/java/com/example/workout/WorkoutDetailFragment.java
+++ app/src/main/java/com/example/workout/WorkoutDetailFragment.java
@@ -13,26 +13,11 @@ import android.view.ViewGroup;
  */
 public class WorkoutDetailFragment extends Fragment {
 
-
-   package com.hfad.workout;
-import android.support.v4.app.Fragment;
-import android.os.Bundle;
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
-    public class WorkoutDetailFragment extends Fragment {
         @Override
         public View onCreateView(LayoutInflater inflater, ViewGroup container,
                                  Bundle savedInstanceState) {
             return inflater.inflate(R.layout.fragment_workout_detail, container, false);
         }
-    }
 
-
-    @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_workout_detail, container, false);
-    }
 
 }
